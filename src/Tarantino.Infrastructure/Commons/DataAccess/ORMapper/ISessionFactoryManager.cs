dd883e0dda37dd1191f589e8484e645578ecdc6d
khurwitz_2000 <khurwitz_2000@yahoo.com>
2008-03-26 15:28:34 +0000

    Making sessionfactorymanager not a singleton

--- src/Tarantino.Infrastructure/Commons/DataAccess/ORMapper/ISessionFactoryManager.cs
+++ src/Tarantino.Infrastructure/Commons/DataAccess/ORMapper/ISessionFactoryManager.cs
@@ -1,11 +1,10 @@
 using NHibernate;
-using NHibernate.Cfg;
 using StructureMap;
 using Tarantino.Core;
 
 namespace Tarantino.Infrastructure.Commons.DataAccess.ORMapper
 {
-	[PluginFamily(ServiceKeys.Default, IsSingleton = true)]
+	[PluginFamily(ServiceKeys.Default)]
 	public interface ISessionFactoryManager
 	{
 		ISessionFactory GetSessionFactory(string connectionStringKey);
