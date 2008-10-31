ba090ab5f8da67d0cdf5a30ce1c4d3a7ffbebcd5
kwhurwitz <kwhurwitz@1249467d-c245-0410-8ced-997fcb3cb136>
2008-10-31 03:48:41 +0000

    Added Excel Import task

--- src/Tarantino.Infrastructure/DatabaseManager/DataAccess/QueryExecutor.cs
+++ src/Tarantino.Infrastructure/DatabaseManager/DataAccess/QueryExecutor.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
-using StructureMap;
-using Tarantino.Core;
 using Tarantino.Core.DatabaseManager.Services;
 using ConnectionSettings=Tarantino.Core.DatabaseManager.Model.ConnectionSettings;
 
@@ -13,7 +11,7 @@ namespace Tarantino.Infrastructure.DatabaseManager.DataAccess
 	
 	public class QueryExecutor : IQueryExecutor
 	{
-		private IConnectionStringGenerator _connectionStringGenerator;
+		private readonly IConnectionStringGenerator _connectionStringGenerator;
 
 		public QueryExecutor(IConnectionStringGenerator connectionStringGenerator)
 		{
@@ -24,9 +22,9 @@ namespace Tarantino.Infrastructure.DatabaseManager.DataAccess
 		{
 			string connectionString = _connectionStringGenerator.GetConnectionString(settings, runAgainstSpecificDatabase);
 
-			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (var connection = new SqlConnection(connectionString))
 			{
-				Server server = new Server(new ServerConnection(connection));
+				var server = new Server(new ServerConnection(connection));
 				server.ConnectionContext.ExecuteNonQuery(sql);
 			}
 		}
@@ -35,21 +33,21 @@ namespace Tarantino.Infrastructure.DatabaseManager.DataAccess
 		{
 			string connectionString = _connectionStringGenerator.GetConnectionString(settings, true);
 
-			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (var connection = new SqlConnection(connectionString))
 			{
-				Server server = new Server(new ServerConnection(connection));
+				var server = new Server(new ServerConnection(connection));
 				return Convert.ToInt32(server.ConnectionContext.ExecuteScalar(sql));
 			}
 		}
 
 		public string[] ReadFirstColumnAsStringArray(ConnectionSettings settings, string sql)
 		{
-			List<string> list = new List<string>();
+			var list = new List<string>();
 			string connectionString = _connectionStringGenerator.GetConnectionString(settings, true);
 
-			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (var connection = new SqlConnection(connectionString))
 			{
-				Server server = new Server(new ServerConnection(connection));
+				var server = new Server(new ServerConnection(connection));
 				using (SqlDataReader reader = server.ConnectionContext.ExecuteReader(sql))
 				{
 					while (reader.Read())
