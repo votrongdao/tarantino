22c08207816bdc42b0e08e59d312efe2b9a61298
eric.hexter <eric.hexter@1249467d-c245-0410-8ced-997fcb3cb136>
2009-09-02 15:24:31 +0000

    Added a console version of the DatabaseManager

---
+++ src/Tarantino.DatabaseManager.Console/ConsoleDatabaseDeployer.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using Tarantino.Core.DatabaseManager.Model;
+using Tarantino.Core.DatabaseManager.Services;
+using Tarantino.Core.DatabaseManager.Services.Impl;
+
+namespace Tarantino.DatabaseManager.Console
+{
+    public class ConsoleDatabaseDeployer:ITaskObserver
+    {
+        IDictionary<string,string> _properties = new Dictionary<string, string>();
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public void SetVariable(string name, string value)
+        {
+            if(_properties.ContainsKey(name))
+            {
+                _properties[name] = value;
+            }
+            else
+            {
+                _properties.Add(name, value);    
+            }
+            
+        }
+
+        public void UpdateDatabase(string Server, string Database, string ScriptDirectory, RequestedDatabaseAction Action)
+        {
+            var manager = new SqlDatabaseManager();
+            var settings = new ConnectionSettings(Server, Database, true, null, null);
+            var taskAttributes = new TaskAttributes(settings, ScriptDirectory)
+                                     {
+                                         RequestedDatabaseAction = Action,
+                                     };
+            try
+            {
+                manager.Upgrade(taskAttributes, this);
+
+                foreach (var property in _properties)
+                {
+                    Log(property.Key +": " + property.Value);
+                }
+
+            }
+            catch (Exception exception)
+            {                
+                var ex = exception;
+                do
+                {
+                    Log("Failure: " + ex.Message);
+                    ex = exception.InnerException;    
+                } while (ex.InnerException!=null);
+
+                Log(exception.ToString());
+                
+
+            }
+            
+        }
+    }
+}
---
+++ src/Tarantino.DatabaseManager.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tarantino.Core.DatabaseManager.Services.Impl;
+using Tarantino.DatabaseManager.Core;
+
+namespace Tarantino.DatabaseManager.Console
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //"DeployDatabase.exe Action(Create|Update) .\SqlExpress CEMS_DEV  .\Database\ [[username] [password]]"
+            if(args.Length==4)
+            {
+                RequestedDatabaseAction Action = (RequestedDatabaseAction) Enum.Parse(typeof(RequestedDatabaseAction), args[0]);
+                string Server = args[1];
+                string Database = args[2];
+                string ScriptDirectory = args[3];
+
+                if (Action != RequestedDatabaseAction.Create && Action != RequestedDatabaseAction.Update)
+                {
+                    System.Console.WriteLine("The Action must be Create or Update");
+                    InvalidArguments();
+                    return;
+                }
+
+                var deployer = new ConsoleDatabaseDeployer();
+                deployer.UpdateDatabase(Server, Database, ScriptDirectory, Action);
+                
+            }
+            else if(args.Length==6)
+            {
+                throw  new NotImplementedException("This should be considnered not secure");
+            }
+            else
+            {
+                InvalidArguments();
+            }
+        }
+
+        private static void InvalidArguments()
+        {
+            System.Console.WriteLine("Invalid Arguments");
+            System.Console.WriteLine(
+                @"DeployDatabase.exe Action(Create|Update) .\SqlExpress DatabaseName  .\DatabaseScripts\ [[username] [password]]");
+        }
+    }
+}
