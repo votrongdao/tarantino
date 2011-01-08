e1ca8bf42198f6729183a8136a6fbcae167e75ab
kwhurwitz <kwhurwitz@1249467d-c245-0410-8ced-997fcb3cb136>
2011-01-08 13:41:03 +0000

    - upgraded NAnt to compile C# 4.0 code - Fixed unit tests

--- src/Tarantino.Deployer.UnitTests/Core/Services/DeploymentFactoryTester.cs
+++ src/Tarantino.Deployer.UnitTests/Core/Services/DeploymentFactoryTester.cs
@@ -17,26 +17,22 @@ namespace Tarantino.Deployer.UnitTests.Core.Services
 		{
 			var mocks = new MockRepository();
 			var clock = mocks.CreateMock<ISystemClock>();
-			var parser = mocks.CreateMock<IRevisionNumberParser>();
-			var calculator = mocks.CreateMock<IDeploymentResultCalculator>();
 
 			using (mocks.Record())
 			{
 				Expect.Call(clock.GetCurrentDateTime()).Return(new DateTime(2007, 4, 15));
-				Expect.Call(parser.Parse("Output...")).Return(785);
-				Expect.Call(calculator.GetResult("Output...")).Return(DeploymentResult.Failure);
 			}
 
 			using (mocks.Playback())
 			{
 				IDeploymentFactory factory = new DeploymentFactory(clock);
-				Deployment deployment = factory.CreateDeployment("A1", "E1", "jsmith", "Output...", "1.0", false);
+				Deployment deployment = factory.CreateDeployment("A1", "E1", "jsmith", "Output...", "1.0", true);
 
 				Assert.That(deployment.Application, Is.EqualTo("A1"));
 				Assert.That(deployment.Environment, Is.EqualTo("E1"));
 				Assert.That(deployment.DeployedBy, Is.EqualTo("jsmith"));
 				Assert.That(deployment.DeployedOn, Is.EqualTo(new DateTime(2007, 4, 15)));
-				Assert.That(deployment.Revision, Is.EqualTo(785));
+				Assert.That(deployment.Revision, Is.EqualTo("1.0"));
 				Assert.That(deployment.Output.Output, Is.EqualTo("Output..."));
 				Assert.That(deployment.Result, Is.SameAs(DeploymentResult.Failure));
 				Assert.That(deployment.Output.Deployment, Is.SameAs(deployment));
